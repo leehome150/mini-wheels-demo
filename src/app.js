55ae441fef8414cbd06c14accddf3f074b170285
leehome150 <750979206@qq.com>
2020-06-13 14:37:58 +0800

    添加4个测试用例

--- src/app.js
+++ src/app.js
@@ -4,15 +4,83 @@ import Icon from './icon'
 import ButtonGroup from './button-group'
 
 Vue.component('g-button', Button)
-Vue.component('g-icon',Icon)
-Vue.component('button-group',ButtonGroup)
+Vue.component('g-icon', Icon)
+Vue.component('button-group', ButtonGroup)
 
 
 new Vue({
     el: '#app',
-    data:{
-        loading1:false,
-        loading2:true,
-        loading3:false,
+    data: {
+        loading1: false,
+        loading2: true,
+        loading3: false,
     }
 })
+
+import chai from 'chai'
+
+const expect = chai.expect
+//单元测试
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting'
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    const href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-setting')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting',
+            loading: true,
+
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    const href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-loading')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting',
+        }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('1')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting',
+            iconPosition: 'right'
+        }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    vm.$el.remove()
+    vm.$destroy()
+}
