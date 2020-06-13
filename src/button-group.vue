97542d9ed36145fd9ce806fed95c6287dfa43dab
leehome150 <750979206@qq.com>
2020-06-13 10:07:38 +0800

    创建button-group组件

---
+++ src/button-group.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="button-group">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+  export default {
+  };
+</script>
+
+<style lang="scss" scoped>
+
+    .button-group{
+        display: inline-flex;
+        vertical-align: middle;
+        >.bt{
+            border-radius:0;
+            margin-left: -1px;
+            &:first-child{
+                border-top-left-radius: 4px;
+                border-bottom-left-radius: 4px;
+            }
+            &:last-child{
+                border-top-right-radius: 4px;
+                border-bottom-right-radius: 4px;
+            }
+            &:hover{
+                position:relative;
+                z-index: 1;
+            }
+        }
+    }
+</style>
