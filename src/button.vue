c3fb05d859c851e17d7f789969d72dfc5d9146fe
leehome150 <750979206@qq.com>
2020-06-13 09:33:12 +0800

    添加click事件

--- src/button.vue
+++ src/button.vue
@@ -1,7 +1,7 @@
 <template>
-    <button class="bt" :class="{[`icon-${iconPosition}`]:true}">
-        <g-icon class="loading" name="loading"></g-icon>
-        <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+    <button class="bt" :class="{[`icon-${iconPosition}`]:true}" @click="$emit('click')">
+        <g-icon class="icon" v-if="icon&&!loading" :name="icon"></g-icon>
+        <g-icon class="icon loading" name="loading" v-if="loading"></g-icon>
         <div class="content">
             <slot/>
         </div>
@@ -14,6 +14,10 @@
         // props: ['icon', 'iconPosition']
         props: {
             icon: {},
+            loading:{
+              type:Boolean,
+              default: false,
+            },
             iconPosition: {
                 type: String,
                 default: 'left',
