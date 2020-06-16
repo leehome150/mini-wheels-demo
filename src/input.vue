d5ba2b191575142dd3d4dcd3a3c5e28a861328d7
leehome150 <750979206@qq.com>
2020-06-16 14:54:21 +0800

    添加input.vue 及其样式

---
+++ src/input.vue
@@ -0,0 +1,92 @@
+<template>
+    <div class="wrapper" :class="{'error':error}">
+        <label>
+            <input :value="value" :disabled="disabled" :readonly="readonly" type="text">
+            <template v-if="error">
+                <g-icon name="error" class="icon-error"></g-icon>
+                <span class="errorMessage">{{error}}</span>
+            </template>
+        </label>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            value: {
+                type: String
+            },
+            disabled: {
+                type: Boolean,
+                default: false
+            },
+            readonly: {
+                type: Boolean,
+                default: false
+            },
+            error: {
+                type: String,
+            }
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+    $height: 32px;
+    $border-color: #999;
+    $border-color-hover: #666;
+    $border-radius: 4px;
+    $font-size: 12px;
+    $box-shadow-color: rgba(0, 0, 0, 0.5);
+    $red: #F1453D;
+    .wrapper {
+        font-size: $font-size;
+        display: inline-flex;
+        align-items: center;
+
+        &.error {
+            > label > input {
+                border-color: $red;
+            }
+
+        }
+
+        > label > input {
+            height: $height;
+            border: 1px solid $border-color;
+            border-radius: $border-radius;
+            padding: 0 8px;
+            font-size: inherit;
+
+
+            &:hover {
+                border-color: $border-color-hover;
+            }
+
+            &:focus {
+                box-shadow: inset 0 1px 3px $box-shadow-color;
+                outline: none;
+            }
+
+            &[disabled] {
+                border-color: #bbb;
+                color: #bbb;
+                cursor: not-allowed;
+
+            }
+
+            &[readonly] {
+                border-color: #999;
+                color: #999;
+            }
+        }
+
+        .icon-error {
+            fill: $red;
+        }
+
+        .errorMessage {
+            color: $red;
+        }
+    }
+</style>
