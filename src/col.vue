4599bc5648085da27b02b868a237914e34e6d79d
leehome150 <750979206@qq.com>
2020-06-17 20:18:58 +0800

    基本实现row 和col的功能

--- src/col.vue
+++ src/col.vue
@@ -1,12 +1,53 @@
 <template>
-    
+    <div class="col" :class="[`col-${span}`, offset &&`offset-${offset}`]"
+         :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}">
+        <div style="border:1px solid red;height:100px">
+            <slot></slot>
+        </div>
+    </div>
 </template>
 
 <script>
-  export default {
-  };
+    export default {
+        data() {
+            return {
+                gutter: 0
+            }
+
+        },
+        props: {
+            span: {
+                type: [Number, String]
+            },
+            offset: {
+                type: [Number, String]
+            }
+        }
+    };
 </script>
 
 <style lang="scss" scoped>
+    .col {
+        height: 100px;
+        width: 50%;
+        $class-prefix: col-;
+
+        @for $n from 1 through 24 {
+
+            &.#{$class-prefix}#{$n} {
+                width: ($n/24)*100%
+
+            }
+        }
+        $class-prefix: offset-;
+
+        @for $n from 1 through 24 {
+
+            &.#{$class-prefix}#{$n} {
+                margin-left: ($n/24)*100%;
+
+            }
+        }
+    }
 
 </style>
--- src/row.vue
+++ src/row.vue
@@ -1,12 +1,26 @@
 <template>
-    
+    <div class="row" :style="{marginLeft:-gutter/2+'px',marginRight:-gutter/2+'px'}">
+        <slot></slot>
+    </div>
 </template>
 
 <script>
-  export default {
-  };
+    export default {
+        props: {
+            gutter: {
+                type: [Number, String]
+            }
+        },
+        mounted() {
+            this.$children.forEach((vm) => {
+                vm.gutter = this.gutter
+            })
+        }
+    };
 </script>
 
 <style lang="scss" scoped>
-
+    .row {
+        display: flex;
+    }
 </style>
