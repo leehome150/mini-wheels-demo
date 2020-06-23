c76c81d27da3ddc81d9631dd93bb17d49b168fb6
leehome150 <750979206@qq.com>
2020-06-23 20:58:24 +0800

    准备push

--- docs/.vuepress/components/buttons-demo.vue
+++ docs/.vuepress/components/buttons-demo.vue
@@ -1,14 +1,34 @@
 <template>
     <div>
         <g-button>默认按钮</g-button>
+        <g-button icon="setting">默认按钮</g-button>
+        <g-button :loading=true>默认按钮</g-button>
+        <g-button disabled>默认按钮</g-button>
+
+        <pre><code >{{content}}</code></pre>
+
     </div>
 </template>
 <script>
     import Button from "../../../src/button"
 
     export default {
-        components:{
-            'g-button':Button
+        data() {
+            return {
+                content: `
+                    <g-button>默认按钮</g-button>
+                    <g-button icon="setting">默认按钮</g-button>
+                    <g-button :loading=true>默认按钮</g-button>
+                    <g-button disabled>默认按钮</g-button>
+
+`
+            }
+        },
+        components: {
+            'g-button': Button
         }
     }
 </script>
+<style lang="scss" scoped>
+
+</style>
