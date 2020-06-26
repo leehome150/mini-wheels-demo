70e8014eafe0e92c1c05c3c05b4eb7ee540ae8f4
leehome150 <750979206@qq.com>
2020-06-26 17:42:38 +0800

    添加图片

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,11 +3,12 @@ module.exports = {
     title: 'MiniWheels UI',
     description: '一个好用的UI框架',
     themeConfig: {
+        logo: '/logo.png',
         search: true,
         searchMaxSuggestions: 10,
         nav: [
             {text: '首页', link: '/'},
-            {text: '指南', link: '/guide/'},
+            {text: '指南', link: '/install/'},
             {text: '交流', link: 'https://google.com'},
             {text: 'GitHub', link: 'https://github.com/leehome150/mini-wheels-demo'}
         ],
--- docs/README.md
+++ docs/README.md
@@ -1,3 +1,12 @@
-# MiniWheels UI 官网
+---
+home: true
+heroImage: /home-page2.jpg
+heroText: MiniWheels UI 官网
+tagline: 一个基于Vue实现的简单好用的UI框架
+actionText: 快速上手 →
+actionLink: /install/
+---
+
+<hr>
 
 MiniWheels UI是一个好用的UI框架，提供了Button、Input、Grid、Layout、Toast组件，适合移动端和PC端使用。   
